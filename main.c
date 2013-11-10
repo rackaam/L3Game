bb4e91ac40b53364393c0574c5c4f6fddc6325c3
hleveque <hleveque@ubuntu.(none)>
2013-11-10 23:52:48 +0100

    Création de la fonction cherchant les differentes chaines de caractère à tester Reste à faire le test Ajout du Dico.c

--- main.c
+++ main.c
@@ -166,6 +166,49 @@ cpBool preSolve(cpArbiter *arb, cpSpace *space, void *data)
     return cpFalse; // Aucune collision avec ce segment
 }
 
+char firstRule(char string)
+{
+    int g,i,j,size=strlen(string);
+
+    char research[20], research2[20];
+    for( i=0;i<size;i++)
+    {
+        memset (research, 0, sizeof (research));
+        memset (research2, 0, sizeof (research2));
+        j=0;
+        g=i;
+
+        while(g<size)
+        {
+            research[j]=string[g];
+
+            j=j+1;
+            g=i+j;
+        }
+
+    //Teste si research est le mot recherché
+        //if ( ->Fonction pour vérifier si le mot est dans le dico<-)
+        //    return research;
+
+
+        j=0;
+        g=size-i-1;
+
+        while(j<=g)
+        {
+            research2[g-j]=string[g-j];
+            j=j+1;
+        }
+
+    //Teste si research2 est le mot recherché
+        //if ( ->Fonction pour vérifier si le mot est dans le dico<-)
+        //    return research2;
+
+
+    }
+
+}
+
 void selection(Circle* circles)
 {
     int i;
@@ -182,6 +225,7 @@ void selection(Circle* circles)
     {
         // Call to function Rules1 !!!
         printf("Selected letters : %s\n", str);
+        char wordFound[20]=firstRule(str);
     }
 }
 
