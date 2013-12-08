ba0ff3e8035b55197db6e7b3f3aa4c346044b01a
rakam <rakam2@free.fr>
2013-12-08 23:02:05 +0100

    Commentaires dans main.c

--- main.c
+++ main.c
@@ -10,7 +10,7 @@ void renderScore(SDL_Surface* surface);
 cpBool preSolve(cpArbiter *arb, cpSpace *space, void *data);
 GSList* selection(GSList* liste, cpVect* startPos, char * (*ruleFunction)(char*, GHashTable*), GHashTable *hashtable, cpSpace* space);
 gint sortFunction(gconstpointer a, gconstpointer b, gpointer startPos);
-void checkCharsDistribution(int count[]);
+void checkCharsDistribution(int count[], char* fileName);
 
 /*Global----------*/
 int score = 0;
@@ -28,6 +28,7 @@ int main(int argc, char* argv[])
     int ballsNb = 40;
     char* (*ruleFunction)(char*, GHashTable*);
 
+    /* Gestion des arguments */
     for(i = 1; i < argc; i++)
     {
         char *key, *value;
@@ -52,8 +53,9 @@ int main(int argc, char* argv[])
     printf("rule: %d\n", rule);
     printf("-----------\n");
 
+    /* Proportion de chaque lettre dans le dico */
     int count[26] = {0};
-    checkCharsDistribution(count);
+    checkCharsDistribution(count, "dico");
     int spawnTabLen = 0;
     for(i = 0; i < 26; i++)
     {
@@ -67,6 +69,8 @@ int main(int argc, char* argv[])
             spawnsTab[k++] = i + 'a';
         }
     }
+
+    /* Création et remplissage de la hashtable */
     GHashTable *hashTable = NULL;
     if(rule == 1)
     {
@@ -85,7 +89,7 @@ int main(int argc, char* argv[])
     }
     else
     {
-        /* Règle 1 par défaut */
+        // Règle 1 par défaut
         hashTable = g_hash_table_new(g_str_hash, g_str_equal);
         ruleFunction = firstRule;
     }
@@ -100,11 +104,12 @@ int main(int argc, char* argv[])
     }
     fclose(file);
 
+    /* Initialisation de la partie graphique */
     TTF_Init();
     circleInit();
     cpVect mouse1 = cpv(-1, -1);
     cpVect mouse2 = cpv(-1, -1);
-    int drawLine = 0;
+    int mouseSeg = 0;
     SDL_Color scoreTextColor = {0, 0, 0};
     SDL_Init( SDL_INIT_VIDEO );
 
@@ -120,6 +125,7 @@ int main(int argc, char* argv[])
 
     cpSpace* space = getSpace();
 
+    /* Création du recipient */
     cpShape* container[3];
     container[0] = cpSegmentShapeNew(space->staticBody, cpv(20, 40),
                                      cpv(70, 450), 0);
@@ -137,6 +143,7 @@ int main(int argc, char* argv[])
     cpSpaceAddShape(space, container[1]);
     cpSpaceAddShape(space, container[2]);
 
+    /* Création des balles */
     GSList* liste = NULL;
     for(i = 0; i < ballsNb; i++)
     {
@@ -158,8 +165,10 @@ int main(int argc, char* argv[])
     int frameCounter = 0;
     while (run)
     {
+        /* RAZ de la surface */
         SDL_FillRect(surface, NULL, SDL_MapRGB(surface->format, 255, 255, 255));
 
+        /* Traitement des évenements (non bloquant) */
         while(SDL_PollEvent(&event))
         {
             switch(event.type)
@@ -174,13 +183,13 @@ int main(int argc, char* argv[])
             case SDL_MOUSEMOTION:
                 if(((SDL_MouseMotionEvent*)&event)->state & SDL_BUTTON(1))
                 {
-                    drawLine = 1;
+                    mouseSeg = 1;
                     mouse2.x = event.button.x;
                     mouse2.y = event.button.y;
                 }
                 break;
             case SDL_MOUSEBUTTONUP:
-                drawLine = 0;
+                mouseSeg = 0;
                 liste = selection(liste, &mouse1, ruleFunction, hashTable, space);
                 break;
             case SDL_KEYDOWN:
@@ -198,36 +207,49 @@ int main(int argc, char* argv[])
 
         g_slist_foreach(liste, resetAffected, NULL);
 
-        cpShape* mouseSeg = NULL;
-        if(drawLine)
+        cpShape* mouseSegShape = NULL;
+
+        /* Création du segment pour les collisions */
+        if(mouseSeg)
         {
-            mouseSeg = cpSegmentShapeNew(space->staticBody,
-                                         cpv(mouse1.x, mouse1.y),
-                                         cpv(mouse2.x, mouse2.y), 0);
-            mouseSeg->collision_type = 1;
-            cpSpaceAddShape(space, mouseSeg);
+            mouseSegShape = cpSegmentShapeNew(space->staticBody,
+                                              cpv(mouse1.x, mouse1.y),
+                                              cpv(mouse2.x, mouse2.y), 0);
+            mouseSegShape->collision_type = 1;
+            cpSpaceAddShape(space, mouseSegShape);
         }
+
+        // Simulation
         cpSpaceStep(space, timeStep);
+
+        // Dessin du récipient
         renderContainer(surface, container, 3);
 
+        // Dessin de chaque cercle
         g_slist_foreach(liste, renderCircle, surface);
 
-        if(drawLine)
+        // Dessin du segment
+        if(mouseSeg)
         {
             lineColor(surface, mouse1.x, mouse1.y,
                       mouse2.x, mouse2.y, 0x000000FF);
-            cpSpaceRemoveShape(space, mouseSeg);
-            cpShapeFree(mouseSeg);
+            cpSpaceRemoveShape(space, mouseSegShape);
+            cpShapeFree(mouseSegShape);
         }
+
+        // Affichage du score
         char buff[15];
         sprintf(buff, "Score : %d", score);
         SDL_Surface* scoreSurface = TTF_RenderText_Blended(font, buff, scoreTextColor);
         SDL_BlitSurface(scoreSurface, NULL, surface, &rect0);
+
+        // Affichage du dernier mot trouvé
         if(time(NULL) - displayedWordTime < 5)
         {
             SDL_Surface* wordSurface = TTF_RenderText_Blended(font, displayedWord, scoreTextColor);
             SDL_BlitSurface(wordSurface, NULL, surface, &rect1);
         }
+
         if(videoRecording)
         {
             char buf[20];
@@ -236,6 +258,7 @@ int main(int argc, char* argv[])
             frameCounter++;
         }
         SDL_Flip(surface);
+        // Mise en pause du programme pour réduire l'occupation du processeur
         SDL_Delay(1000.0 / 60.0);
     }
 
@@ -248,6 +271,7 @@ int main(int argc, char* argv[])
     return 0;
 }
 
+/* Retourne un cpSpace configuré pour le jeu */
 cpSpace* getSpace(void)
 {
     cpSpace *space = cpSpaceNew();
@@ -256,6 +280,7 @@ cpSpace* getSpace(void)
     return space;
 }
 
+/* Affiche les nbShape segments du récipient */
 void renderContainer(SDL_Surface* surface, cpShape** container, int nbShape)
 {
     int i;
@@ -267,6 +292,7 @@ void renderContainer(SDL_Surface* surface, cpShape** container, int nbShape)
     }
 }
 
+/* Fonction de collision entre le segment et une boule */
 cpBool preSolve(cpArbiter *arb, cpSpace *space, void *data)
 {
     cpShape *a, *b;
@@ -275,6 +301,7 @@ cpBool preSolve(cpArbiter *arb, cpSpace *space, void *data)
     return cpFalse; // Aucune collision avec ce segment
 }
 
+/* Recherche un mot dans le dictionnaire et suprime les boules utilisées */
 GSList* selection(GSList* liste, cpVect* startPos, char * (*ruleFunction)(char*,
                   GHashTable*), GHashTable *hashtable, cpSpace* space)
 {
@@ -324,6 +351,7 @@ GSList* selection(GSList* liste, cpVect* startPos, char * (*ruleFunction)(char*,
     return liste;
 }
 
+/* Fonction de tri. Place la boule la plus proche du début du segment en première position */
 gint sortFunction(gconstpointer a, gconstpointer b, gpointer startPos)
 {
     cpVect* start = (cpVect*) startPos;
@@ -332,9 +360,10 @@ gint sortFunction(gconstpointer a, gconstpointer b, gpointer startPos)
     return (int)(cpvdistsq(*start, vA) - cpvdistsq(*start, vB));
 }
 
-void checkCharsDistribution(int count[])
+/* Calcul la répartition de chaque lettre dans un fichier */
+void checkCharsDistribution(int count[], char* fileName)
 {
-    FILE* file = fopen("dico", "r");
+    FILE* file = fopen(fileName, "r");
     char c;
     unsigned int total = 0;
     while((c = fgetc(file)) != EOF)
